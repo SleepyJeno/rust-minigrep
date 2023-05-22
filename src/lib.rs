890f5deac2c47e6ae8ced5085ff7f436a5d6503e
SleepyJeno <kalinskij.zhenya@gmail.com>
2023-05-22 16:38:40 -0400

    added the search and run functions; first working commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{env};
+use std::{env, error, fs, io::{self, BufRead}};
 
 #[derive(Debug)]
 pub struct Config {
@@ -7,10 +7,12 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(query: String, filename: String) -> Config {
-        if env::args().len() < 3 {
+    pub fn build(args: &[String]) -> Config {
+        if args.len() < 3 {
             panic!("Either the search string or the filename are missing");
         }
+        let query = args[1].clone();
+        let filename = args[2].clone();
         Config{
             query,
             filename
@@ -18,15 +20,47 @@ impl Config {
     }
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results: Vec<&str> = vec![];
+
+    for line in contents.lines(){
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn error::Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    let results = search(&config.query, &contents);
+
+    for line in results {
+        println!("{}", line);
+    }
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     #[should_panic]
+    #[allow(unused_variables)]
     fn check_args_provided(){
-        use super::*;
-        let query = "".to_string();
-        let filename = "file.txt".to_string();
-        let _config = Config::build(query.clone(), filename);
+        let args: Vec<String> = vec!["only-string".to_string()];
+        let config = Config::build(&args[..]);
+    }
+
+    #[test]
+    fn one_results() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use minigrep::*;
+use std::env;
 
 fn main() {
     //the program accepts 2 positional arguments:
     //string to search for and a filename to search in 
     //optional parameter - IGNORE_CASE allows for case insensitive pattern matching
 
-    let query = "test".to_string();
-    let filename = "file.txt".to_string();
-
-    let config = Config::build(query, filename);
-
-    println!("{:?} {:?}", config.query, config.filename);
-
     //parse cli args and match as appropriate
     //iterate over a file and check for a partial match in EACH line
     //return all lines that contain a match and print
+
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args);
+
+    minigrep::run(config);
+
 }
